432cb2dde4a7e6328ff833633ab8310224941dd6
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-08-22 21:58:17 +0600

    Prim Kruskal Algorithm Report Added

--- Kruskal_MST.cpp
+++ Kruskal_MST.cpp
@@ -760,79 +760,79 @@ int main()
 		cin >> a >> b >> c;
 		g1.addEdge(a, b, c);
 	}
-	cout<<v<<" "<<e<<" ";
-	auto start1 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_PC_UR_STL();
-    auto stop1 = high_resolution_clock::now();
-    auto duration1 = duration_cast<microseconds>(stop1 - start1);
-    cout<<" "<< duration1.count();
+	// cout<<v<<" "<<e<<" ";
+	// auto start1 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_PC_UR_STL();
+    // auto stop1 = high_resolution_clock::now();
+    // auto duration1 = duration_cast<microseconds>(stop1 - start1);
+    // cout<<" "<< duration1.count();
 
-	auto start2 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_PC_STL();
-    auto stop2 = high_resolution_clock::now();
-    auto duration2 = duration_cast<microseconds>(stop2 - start2);
-    cout<<" "<< duration2.count();
+	// auto start2 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_PC_STL();
+    // auto stop2 = high_resolution_clock::now();
+    // auto duration2 = duration_cast<microseconds>(stop2 - start2);
+    // cout<<" "<< duration2.count();
 
-	auto start3 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_UR_STL();
-    auto stop3 = high_resolution_clock::now();
-    auto duration3 = duration_cast<microseconds>(stop3 - start3);
-    cout<<" "<< duration3.count();
+	// auto start3 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_UR_STL();
+    // auto stop3 = high_resolution_clock::now();
+    // auto duration3 = duration_cast<microseconds>(stop3 - start3);
+    // cout<<" "<< duration3.count();
 
-	auto start4 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_STL();
-    auto stop4 = high_resolution_clock::now();
-    auto duration4 = duration_cast<microseconds>(stop4 - start4);
-    cout<<" "<< duration4.count();
+	// auto start4 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_STL();
+    // auto stop4 = high_resolution_clock::now();
+    // auto duration4 = duration_cast<microseconds>(stop4 - start4);
+    // cout<<" "<< duration4.count();
 
 
-	auto start5 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_PC_UR_LinkedList();
-    auto stop5 = high_resolution_clock::now();
-    auto duration5 = duration_cast<microseconds>(stop5 - start5);
-    cout<<" "<< duration5.count();
+	// auto start5 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_PC_UR_LinkedList();
+    // auto stop5 = high_resolution_clock::now();
+    // auto duration5 = duration_cast<microseconds>(stop5 - start5);
+    // cout<<" "<< duration5.count();
 
-	auto start6 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_PC_LinkedList();
-    auto stop6 = high_resolution_clock::now();
-    auto duration6 = duration_cast<microseconds>(stop6 - start6);
-    cout<<" "<< duration6.count();
+	// auto start6 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_PC_LinkedList();
+    // auto stop6 = high_resolution_clock::now();
+    // auto duration6 = duration_cast<microseconds>(stop6 - start6);
+    // cout<<" "<< duration6.count();
 
-	auto start7 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_UR_LinkedList();
-    auto stop7 = high_resolution_clock::now();
-    auto duration7 = duration_cast<microseconds>(stop7 - start7);
-    cout<<" "<< duration7.count();
+	// auto start7 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_UR_LinkedList();
+    // auto stop7 = high_resolution_clock::now();
+    // auto duration7 = duration_cast<microseconds>(stop7 - start7);
+    // cout<<" "<< duration7.count();
 
-	auto start8 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_LinkedList();
-    auto stop8 = high_resolution_clock::now();
-    auto duration8 = duration_cast<microseconds>(stop8 - start8);
-    cout<<" "<< duration8.count();
+	// auto start8 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_LinkedList();
+    // auto stop8 = high_resolution_clock::now();
+    // auto duration8 = duration_cast<microseconds>(stop8 - start8);
+    // cout<<" "<< duration8.count();
 
-	auto start9 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_PC_UR_Array();
-    auto stop9 = high_resolution_clock::now();
-    auto duration9 = duration_cast<microseconds>(stop9 - start9);
-    cout<<" "<< duration9.count();
+	// auto start9 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_PC_UR_Array();
+    // auto stop9 = high_resolution_clock::now();
+    // auto duration9 = duration_cast<microseconds>(stop9 - start9);
+    // cout<<" "<< duration9.count();
 
-	auto start10 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_PC_Array();
-    auto stop10 = high_resolution_clock::now();
-    auto duration10 = duration_cast<microseconds>(stop10 - start10);
-    cout<<" "<< duration10.count();
+	// auto start10 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_PC_Array();
+    // auto stop10 = high_resolution_clock::now();
+    // auto duration10 = duration_cast<microseconds>(stop10 - start10);
+    // cout<<" "<< duration10.count();
 
-	auto start11 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_UR_Array();
-    auto stop11 = high_resolution_clock::now();
-    auto duration11 = duration_cast<microseconds>(stop11 - start11);
-    cout<<" "<< duration11.count();
+	// auto start11 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_UR_Array();
+    // auto stop11 = high_resolution_clock::now();
+    // auto duration11 = duration_cast<microseconds>(stop11 - start11);
+    // cout<<" "<< duration11.count();
 
-	auto start12 = high_resolution_clock::now();
-    g1.kruskal_mst_DSU_Array();
-    auto stop12 = high_resolution_clock::now();
-    auto duration12 = duration_cast<microseconds>(stop12 - start12);
-    cout<<" "<< duration12.count()<<endl;
+	// auto start12 = high_resolution_clock::now();
+    // g1.kruskal_mst_DSU_Array();
+    // auto stop12 = high_resolution_clock::now();
+    // auto duration12 = duration_cast<microseconds>(stop12 - start12);
+    // cout<<" "<< duration12.count()<<endl;
 
 
 	return 0;
--- Random_Graph_Generation.py
+++ Random_Graph_Generation.py
@@ -3,28 +3,23 @@ import sys
 import random
 import os
 
-#Generate
-for i in range(20):
-    nvertex = random.randint(100,200)
-    G = nx.erdos_renyi_graph(nvertex, 0.5, seed=123, directed=False)
-    for (u,v,w) in G.edges(data=True):
-        w['weight'] = random.randint(0,10000)
+# Generate
+nvertex = random.randint(100, 200)
+G = nx.erdos_renyi_graph(nvertex, 0.5, seed=123, directed=False)
+for (u, v, w) in G.edges(data=True):
+    w['weight'] = random.randint(0, 10000)
 
 # Print
-    file_path = 'inp.txt'
-    sys.stdout = open(file_path, "w")
+file_path = 'inp.txt'
+sys.stdout = open(file_path, "w")
 
-    print(nx.number_of_nodes(G), end=" ")
-    print(nx.number_of_edges(G))
-    for line in nx.generate_edgelist(G, data=["weight"]):
-        print(line)
+print(nx.number_of_nodes(G), end=" ")
+print(nx.number_of_edges(G))
+for line in nx.generate_edgelist(G, data=["weight"]):
+    print(line)
 
-#Test With Code
-    str = 'Kruskal_MST'
-    str2= 'Prim_MST'
-    #os.system(f"cd '../Prim-Kruskal-Graph-Implementation/' && g++ {str}.cpp -o {str} && '../Prim-Kruskal-Graph-Implementation/'{str}")
-    os.system(f"cd '../Prim-Kruskal-Graph-Implementation/' && g++ {str2}.cpp -o {str2} && '../Prim-Kruskal-Graph-Implementation/'{str2}")
-
-
-
-
+# Test With Code
+str = 'Kruskal_MST'
+str2 = 'Prim_MST'
+os.system(f"cd '../Prim-Kruskal-Graph-Implementation/' && g++ {str}.cpp -o {str} && '../Prim-Kruskal-Graph-Implementation/'{str}")
+os.system(f"cd '../Prim-Kruskal-Graph-Implementation/' && g++ {str2}.cpp -o {str2} && '../Prim-Kruskal-Graph-Implementation/'{str2}")
