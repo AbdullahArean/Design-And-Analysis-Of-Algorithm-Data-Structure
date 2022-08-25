93109c9e70327a8f288cbdec0295aafaeac62703
Abdullah Ibne Hanif Arean <70502574+AbdullahArean@users.noreply.github.com>
2022-08-25 17:24:30 +0600

    Update Code.cpp

--- graphleetcode.cpp
+++ graphleetcode.cpp
@@ -1,28 +1,47 @@
-int snakesAndLadders(vector<vector<int>>& board) {
-        queue<int> container;
-        int n=board.size();
-        n=n*n+1;
-        int vis[n];
-        int dis[n];
-        for(int i=0; i<n; i++){
-            vis[n]=0;
-            dis= INT_MAX;
-        }
-        container.push(1);
-        dis[1]=0;
-        vis[1]=1;
-        while(container.empty()){
-            int u= container.front();
-             container.pop();
-            for(int i=1; i<=6; i++){
-                v= u+1;
-                if(vis[v]==0){
-                vis[v]=1;
-                dis[v]=dis[u]+1;
-                container.push(v);
-                }
-            }
-            
-        }
-        
+#include <bits/stdc++.h>
+using namespace std;
+int snakesAndLadders(vector<vector<int>> &board)
+{
+    queue<int> container;
+    int nn = board.size();
+    int n = nn * nn + 1;
+    int vis[n];
+    int dis[n];
+    for (int i = 0; i < n; i++)
+    {
+        vis[i] = 0;
+        dis[i] = INT_MAX;
     }
+    container.push(1);
+    dis[1] = 0;
+    vis[1] = 1;
+    while (container.empty())
+    {
+
+        int u = container.front();
+        container.pop();
+        for (int i = 1; i <= 6; i++)
+        {
+            int v = u + i;
+            int pos_n = v / nn,pos_m;
+            if (pos_n % 2 == 0)
+            {
+                pos_m = (nn - v % nn);
+            }
+            else
+            {
+                pos_m = (v % nn);
+            }
+            int val = board[pos_n][pos_m];
+            if (val != -1)
+                v = val;
+
+            if (vis[v] == 0)
+            {
+                vis[v] = 1;
+                dis[v] = dis[u] + 1;
+                container.push(v);
+            }
+        }
+    }
+}
