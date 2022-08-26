9d2755437a710bc7d2cf0e9012c7877d6dd2bbc0
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-08-26 11:21:33 +0600

    Prim Kruskal Repository Turning into my ultimate repository for graph

--- Graph Problem Solving Coding Minutes/validbfs_final.cpp
+++ Graph Problem Solving Coding Minutes/validbfs_final.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define LL long long
+map<int,int> mp[200005];
+int n, ind = 1, p[200005], x, y;
+ 
+int valid_bfs2() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    cin >> n;
+    for(int i = 0; i < n-1; i++) {
+        cin >> x >> y;
+        mp[x][y]++, ++mp[y][x]; 
+    }  
+ 
+    for(int i = 0; i < n; i++) cin >> p[i];
+ 
+    for(int i = 0; i < n; i++) while(mp[p[i]][p[ind]]) ind++;
+ 
+    cout << (ind == n && p[0] == 1 ? "Yes" : "No");
+}
 class Edge
 {
-
+ 
 public:
     int data;
     set<int> nbrs;
@@ -17,7 +36,7 @@ private:
     unordered_map<int, Edge *> ma;
     int nv;
     int *vis;
-
+ 
 public:
     Graph(int n)
     {
@@ -52,14 +71,14 @@ public:
         int pos = 0;
         // Initialization of necessary Arrays
         vis = new int[nv*2];
-
+ 
         // Insert Value
         for (int i = 1; i <= nv; i++)
         {
             vis[i] = 0;        // 0 Not Visited // 1 Visited// 2 Finish visiting
         }
         queue<int> container;
-
+ 
         vis[given[0]] = 1;
         container.push(given[0]);
         pos++;
@@ -78,12 +97,12 @@ public:
             }
             vis[u] = 2;
         }
-        if (pos == nv && nv!=2)
+        if (pos == nv && given[0]==1)
             return "Yes";
         return "No";
     }
 };
-
+ 
 int main()
 {
    //freopen("inp.txt", "r", stdin);
@@ -103,34 +122,6 @@ int main()
     {
         scanf("%d", &temp);
         list.push_back(temp);
-    }
-    if(ve==2){
-        if(list[0]==2){
-            cout<<"No"<<endl;
-        }
-        else{
-             cout<<"Yes"<<endl;
-
-        }
-        return 0;
-    }
-    if(ve==3){
-        if(list[0]==2 || list[0]==3){
-            cout<<"No"<<endl;
-            return 0;
-        }
-    }
-    if(ve==4){
-        if(list[0]==2|| list[0]==4){
-            cout<<"No"<<endl;
-            return 0;
-        }
-    }
-    if(ve==5){
-        if(list[0]==5|| list[0]==2){
-            cout<<"No"<<endl;
-            return 0;
-        }
     }
    cout<<g1.bfs(list)<<endl;
 }
