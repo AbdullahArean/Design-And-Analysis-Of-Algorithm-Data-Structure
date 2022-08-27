8a9aa65ca023d0225a65c5578ce2a8952eec3505
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-08-27 13:58:16 +0600

    solved completely

--- Leetcode/1539.kth-missing-positive-number.cpp
+++ Leetcode/1539.kth-missing-positive-number.cpp
@@ -1,3 +1,27 @@
+/*
+Explanation
+
+Assume the final result is x,
+And there are m number not missing in the range of [1, x].
+Binary search the m in range [0, A.size()].
+
+If there are m number not missing,
+that is A[0], A[1] .. A[m-1],
+the number of missing under A[m] is A[m] - 1 - m.
+
+If A[m] - 1 - m < k, m is too small, we update left = m.
+If A[m] - 1 - m >= k, m is big enough, we update right = m.
+
+Note that, we exit the while loop, l = r,
+which equals to the number of missing number used.
+So the Kth positive number will be l + k.
+
+
+Complexity
+
+Time O(logN)
+Space O(1)
+*/
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
